7143dc4b3bf6f026e6c82f6d3c99fcb24030f4e9
Lukáš Šálek <saleklukas@gmail.com>
2017-10-09 16:25:17 +0200

    spotify update

--- bin/application-aliases.sh
+++ bin/application-aliases.sh
@@ -3,6 +3,10 @@ alias sps="__spotifyStop"
 alias spu="__spotifyVolumeUp"
 alias spd="__spotifyVolumeDown"
 alias spq="__spotifyQuit"
+alias sp-n="__spotifyNext"
+alias sp-p="__spotifyPrevious"
+alias spi="__spotifyInfo"
+alias spj="__spotifyJump"
 
 function __spotifyPlay() {
   spotify play
@@ -19,12 +23,28 @@ function __spotifyVolumeDown() {
 function __spotifyQuit() {
   spotify quit
 }
+function __spotifyNext() {
+  spotify next
+}
+function __spotifyPrevious() {
+  spotify prev
+}
+function __spotifyInfo() {
+  spotify info
+}
+function __spotifyJump() {
+  spotify pos $1
+}
 __showAppAliases(){
   echo "\n\t===================== Aliases -just write "
   echo "\n\t====== SPOTIFY"
+  echo "spi \t spotify info"
+  echo "spq \t spotify quit"
   echo "spp \t spotify play"
   echo "sps \t spotify stop"
   echo "spu \t spotify volume up"
   echo "spd \t spotify volume down"
-  echo "spq \t spotify quit"
+  echo "sp-n \t spotify next song"
+  echo "sp-p \t spotify previous"
+  echo "spj \t spotify jump at time [sec]"
 }
